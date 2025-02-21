6b36c14e35c6599c1d70605c3916af1d0556ae3f
vmacena-softable <vinicius@softable.com.br>
2025-02-21 18:45:59 -0300

    fix: update server log message to include author name

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import app from './app';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
+    console.log(`server is running on port by Vinicius Macena ${PORT}`);
 });
